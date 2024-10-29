ec28998427d1e88e50b76ba3c9ba29118b399a58
Omar Gamal Hamed Agamy <agamyo168@gmail.com>
2024-10-29 23:01:51 +0300

    add express api app

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const express = require("express");
-const path = require("path");
+const { products } = require("./data");
 
 const app = express();
-// setup static and middleware
-app.use(express.static("./public"));
-// app.get("/", (req, res) => {
-//   res.sendFile("./index.html");
-//   1- add index html to public
-//   2- server-side rendering (template engine)
-// });
+
+app.get("/", (req, res) => {
+  res.send("<h1> Home Page </h1><a href='/api/products'>products</a>");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productId)
+  );
+  if (singleProduct === undefined)
+    return res.status(404).send("<h1>Product doesn't exist</h1>");
+  res.json(singleProduct);
+});
+//we can absolutely join both /v1/query and /products to be the same .get if there's no query string.
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search)
+    sortedProducts = sortedProducts.filter((product) =>
+      product.name.startsWith(search)
+    );
+  if (limit) sortedProducts = sortedProducts.slice(0, Number(limit));
+  if (sortedProducts.length < 1) {
+    // return res
+    //   .status(200)
+    //   .send("<h1>There's no product with this search param</h1>");
+    return res.status(200).json({ success: true, data: [] }); // you can shape the response as much as you desire.
+  }
+
+  res.status(200).json(sortedProducts);
+});
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+  res.send("<h1>This resource doesn't exist</h1>");
 });
 app.listen(5000, () => {
   console.log("SERVER IS LISTENING ON PORT 5000...");
---
+++ express/2-first-express-server/2-navbar-example.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+// setup static and middleware
+app.use(express.static("./public"));
+//You don't need to write this anymore because .static() will render index.html
+//on root.
+// app.get("/", (req, res) => {
+//   res.sendFile("./index.html");
+//   1- add index html to public
+//   2- server-side rendering (template engine)
+// });
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+app.listen(5000, () => {
+  console.log("SERVER IS LISTENING ON PORT 5000...");
+});
